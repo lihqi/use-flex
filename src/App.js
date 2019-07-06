8dfb57e2f8d65b4c74f6cbbae03773442e897fa0
lihqi <455711093@qq.com>
2019-07-06 14:58:59 +0800

    fk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState("赛风赛森送扥森孙");
+    const [val, setVal] = useState("赛风赛森送扥森孙赛风赛森送扥森孙");
+    const [flexA, setFlexA] = useState("赛风赛森送扥森孙");
+    const [flexB, setFlexB] = useState("赛风赛");
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="container ">
+                    <span className="value ellipsis">{value}</span>
+                    <span className="val ellipsis">{val}</span>
+                </div>
+                <input
+                    type="text"
+                    onChange={e => {
+                        setValue(e.target.value);
+                    }}
+                    value={value}
+                />
+                <input
+                    type="text"
+                    onChange={e => {
+                        setVal(e.target.value);
+                    }}
+                    value={val}
+                />
+            </header>
+            <div className="container flex">
+                <span className="flexA ellipsis">{flexA}</span>
+                <span className="flexB ">{flexB}</span>
+            </div>
+            <input
+                type="text"
+                onChange={e => {
+                    setFlexA(e.target.value);
+                }}
+                value={flexA}
+            />
+            <input
+                type="text"
+                onChange={e => {
+                    setFlexB(e.target.value);
+                }}
+                value={flexB}
+            />
+        </div>
+    );
 }
 
 export default App;
